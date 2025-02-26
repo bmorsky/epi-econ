f693c0863f6df7a3e29f5e9aacb9aae1a664be6b
Tyler Meadows <tam15@queensu.ca>
2025-02-26 16:05:15 -0500

    DAE solver

---
+++ epiecon expanded model 4.jl
@@ -0,0 +1,220 @@
+## Expanded Model 4
+
+using Plots, LaTeXStrings
+using NLsolve, DiffEqCallbacks, OrdinaryDiffEq, LinearAlgebra
+using Parameters
+# Parameters using Named Tuples for 
+T = 600 # final time
+p = (α = 0.5, β₁ = 0.4, β₂ = 0.3, βᵤ = 0.2, γ =  1/7, λ =  0.0033, μ =  0.072, ρ = 0.01,
+     b = 0.71, c₁ = 0.1, c₂ = 0.1, d = 1, r = 0.02, y₁ = 1.1,y₂ = 1.0,bg = 0.0)
+p_gig = merge(p,(;bg = 0.01)) # Copy p but change bg
+p_quar = merge(p,(;λ = 1.5*0.0033)) # Copy p but change λ
+
+# Matching parameters and functions
+η = 0.5
+μ = 0.072
+P(Θ) = μ*abs(Θ)^(1-η)
+Q(Θ) = μ*abs(Θ)^(-η)
+
+# System of DAEs
+function epiecon_ode!(du, u, p, t)
+    S₁,I₁,R₁,S₂,I₂,R₂,Sᵤ,Iᵤ,Rᵤ,Θ₁,Θ₂,Vfes,Vfgs,Vfei,Vfgi,Vfer,Vfgr= u
+    @unpack α,β₁,β₂,βᵤ,γ,λ,μ,ρ,b,c₁,c₂,d,r,y₁,y₂ = p # Unpack macro to assign appropriate values to parameters
+
+    Iₜ = I₁ + I₂ + Iᵤ
+    τ = Sᵤ/(Sᵤ+Rᵤ)
+
+    # vars = Ves, Vei, Ver, Vus, Vui, Vur, Vgs, Vgi, Vgr, Θ₁, Θ₂
+
+    Γs = P(Θ₁)*Vfes + P(Θ₂)*Vfgs
+    Γr = P(Θ₁)*Vfer + P(Θ₂)*Vfgr
+    VusVui = ((1-α)*d*(r+ρ) + α*((r+λ+ρ)*Γs - γ*Γr))/((1-α)*((r+γ)*(r+ρ) + (r+λ+ρ)*βᵤ*Iₜ))
+
+    # Wages
+    Wes = α*y₁ + (1 - α)*b + (1 − α)*(β₁ - βᵤ)*Iₜ*(VusVui) + α*Γs
+    Wer = α*y₁ + (1 - α)*b + α*(P(Θ₁)*Vfer + P(Θ₂)*Vfgr)
+    Wgs = α*y₂ + (1 - α)*b + (1 − α)*(β₂ - βᵤ)*Iₜ*(VusVui) + α*Γs
+    Wgr = α*y₂ + (1 - α)*b + α*(P(Θ₁)*Vfer + P(Θ₂)*Vfgr)
+    Wi = (1 − α)*b
+
+    ## Differential equations
+    du[1] = dS₁ = ρ*R₁ - β₁*S₁*(I₁+I₂+Iᵤ) + P(Θ₁)*Sᵤ - λ*S₁
+    du[2] = dI₁ = β₁*S₁*(I₁+I₂+Iᵤ) - (γ+λ)*I₁ #+ P(Θ₁)*Iᵤ
+    du[3] = dR₁ = γ*I₁ - ρ*R₁ + P(Θ₁)*Rᵤ - λ*R₁
+    du[4] = dS₂ = ρ*R₂ - β₂*S₂*(I₁+I₂+Iᵤ) + P(Θ₂)*Sᵤ - λ*S₂
+    du[5] = dI₂ = β₂*S₂*(I₁+I₂+Iᵤ) - (γ+λ)*I₂ #+ P(Θ₂)*Iᵤ
+    du[6] = dR₂ = γ*I₂ - ρ*R₂ + P(Θ₂)*Rᵤ - λ*R₂
+    du[7] = dSᵤ = ρ*Rᵤ - βᵤ*Sᵤ*(I₁+I₂+Iᵤ) - (P(Θ₁)+P(Θ₂))*Sᵤ + λ*S₁+λ*S₂
+    du[8] = dIᵤ = βᵤ*Sᵤ*(I₁+I₂+Iᵤ) - γ*Iᵤ + λ*I₁ + λ*I₂ #- (P(Θ₁)+P(Θ₂))*Iᵤ
+    du[9] = dRᵤ = γ*Iᵤ - ρ*Rᵤ - (P(Θ₁)+P(Θ₂))*Rᵤ + λ*R₁ + λ*R₂
+    # Algebraic Equations
+    du[10] = dΘ₁ =  (r + λ)*c₁/Q(Θ₁) + (1 − α)*(b − y₁) + α*(c₁*Θ₁ + c₂*Θ₂) + τ*(1-α)*(β₁ − βᵤ)*Iₜ*(VusVui) + (1-τ)*ρ*(Vfer-Vfes) + τ*β₁*Iₜ*(Vfes-Vfei)
+    du[11] = dΘ₂ = (r + λ)*c₂/Q(Θ₂) + (1 − α)*(b − y₂) + α*(c₁*Θ₁ + c₂*Θ₂) + τ*(1-α)*(β₂ - βᵤ)*Iₜ*(VusVui) + (1-τ)*ρ*(Vfgr-Vfgs) + τ*β₂*Iₜ*(Vfgs-Vfgi)
+    du[12] = dVfes = y₁ - Wes - β₁*Iₜ*(Vfes - Vfei) - (r+λ)*Vfes
+    du[13] = dVfgs = y₂ - Wgs - β₂*Iₜ*(Vfgs - Vfgi) - (r+λ)*Vfgs
+    du[14] = dVfei = -Wi - γ*(Vfei - Vfer) - (λ+r)*Vfei
+    du[15] = dVfgi = -Wi - γ*(Vfgi - Vfgr) - (λ+r)*Vfgi
+    du[16] = dVfer = y₁ - Wer - ρ*(Vfer - Vfes) - (r+λ)*Vfer
+    du[17] = dVfgr = y₂ - Wgr - ρ*(Vfgr - Vfgs) - (r+λ)*Vfgr
+end
+tspan = (0,T)
+# Algebraic Equations
+function equations!(F, vars, p,u)
+    @unpack α,β₁,β₂,βᵤ,γ,λ,μ,ρ,b,c₁,c₂,d,r,y₁,y₂ = p
+    τ = u[7]/(u[7]+u[9])
+    I₀ = sum(u[[2,5,8]])
+    z1,z2,z3,z4,z5,z6,z7,z8 = vars
+
+    Θ₁ = z1
+    Θ₂ = z2
+    Vfes = z3
+    Vfgs = z4
+    Vfei = z5
+    Vfgi = z6
+    Vfer = z7
+    Vfgr = z8
+
+
+    # Vfes, Vfgs, Vfei, Vfgi, Vfer, Vfgr, Θ₁, Θ₂ = vars
+
+    Γs = P(Θ₁)*Vfes + P(Θ₂)*Vfgs
+    Γr = P(Θ₁)*Vfer + P(Θ₂)*Vfgr
+    VusVui = ((1-α)*d*(r+ρ) + α*((r+λ+ρ)*Γs - γ*Γr))/((1-α)*((r+γ)*(r+ρ) + (r+λ+ρ)*βᵤ*I₀))
+
+    # Wages
+    Wes = α*y₁ + (1 - α)*b + (1 − α)*(β₁ - βᵤ)*I₀*(VusVui) + α*Γs
+    Wer = α*y₁ + (1 - α)*b + α*(P(Θ₁)*Vfer + P(Θ₂)*Vfgr)
+    Wgs = α*y₂ + (1 - α)*b + (1 − α)*(β₂ - βᵤ)*I₀*(VusVui) + α*Γs
+    Wgr = α*y₂ + (1 - α)*b + α*(P(Θ₁)*Vfer + P(Θ₂)*Vfgr)
+    Wi = (1 − α)*b
+
+    F[1] = (r + λ)*c₁/Q(Θ₁) + (1 − α)*(b − y₁) + α*(c₁*Θ₁ + c₂*Θ₂) + τ*(1-α)*(β₁ − βᵤ)*I₀*(VusVui) + (1-τ)*ρ*(Vfer-Vfes) + τ*β₁*I₀*(Vfes-Vfei)
+    F[2] = (r + λ)*c₂/Q(Θ₂) + (1 − α)*(b − y₂) + α*(c₁*Θ₁ + c₂*Θ₂) + τ*(1-α)*(β₂ - βᵤ)*I₀*(VusVui) + (1-τ)*ρ*(Vfgr-Vfgs) + τ*β₂*I₀*(Vfgs-Vfgi)
+    F[3] = y₁ - Wes - β₁*I₀*(Vfes - Vfei) - (r+λ)*Vfes
+    F[4] = y₂ - Wgs - β₂*I₀*(Vfgs - Vfgi) - (r+λ)*Vfgs
+    F[5] = -Wi - γ*(Vfei - Vfer) - (λ+r)*Vfei
+    F[6] = -Wi - γ*(Vfgi - Vfgr) - (λ+r)*Vfgi
+    F[7] = y₁ - Wer - ρ*(Vfer - Vfes) - (r+λ)*Vfer
+    F[8] = y₂ - Wgr - ρ*(Vfgr - Vfgs) - (r+λ)*Vfgr  
+end
+
+
+
+
+# Mass Matrix 
+# DifferentialEquations.jl will solve systems of the type 
+# M(du/dt) = f(u(t)) as a DAE
+M = zeros(17,17)
+for j in 1:9
+    M[j,j] = 1.0
+end
+
+## Disease Free dynamics
+
+# Disease Variable ICs [S₁,I₁,R₁,S₂,I₂,R₂,Sᵤ,Iᵤ,Rᵤ]
+u₀ = [0.905, 0.0, 0.0, 0.07, 0.0, 0.0, 0.025, 0.0, 0.0]
+# Economic Variable ICs [Θ₁,Θ₂,Vfes,Vfgs,Vfei,Vfgi,Vfer,Vfgi]
+v₀ = [2.5, 0.001, 250, 250, 250, 250, 250, 250] # Initial guess
+
+v = nlsolve((F,x) -> equations!(F,x,p,u₀),v₀,ftol=1e-14)
+u = [u₀;v.zero]
+
+DAE = ODEFunction(epiecon_ode!,mass_matrix=M)
+prob = ODEProblem(DAE, u, tspan, p)
+sol = solve(prob,saveat=1:T)
+
+E_DF = sol[1,:]+sol[2,:]+sol[3,:]
+G_DF = sol[4,:]+sol[5,:]+sol[6,:]
+U_DF = sol[7,:]+sol[8,:]+sol[9,:]
+Θe_DF = sol[10,:]
+Θg_DF = sol[11,:]
+
+# 1% initially infected
+# Disease variable ICs [S₁,I₁,R₁,S₂,I₂,R₂,Sᵤ,Iᵤ,Rᵤ]
+u₀ = [0.905*0.99, 0.905*0.01, 0.0, 0.07*0.99, 0.07*0.01, 0.0, 0.025*0.99, 0.025*0.01, 0.0]
+# Economic variable ICs [Θ₁,Θ₂,Vfes,Vfgs,Vfei,Vfgi,Vfer,Vfgi]
+v₀ = [2.5, 0.001, 250, 2.0, 2.50, 2.50, 2.50, 2.50] # Initial guess
+v = nlsolve((F,x) -> equations!(F,x,p,u₀),v₀)
+u = [u₀;v.zero]
+
+prob = ODEProblem(DAE, u, tspan, p)
+sol = solve(prob, saveat=1:T)
+
+E = sol[1,:]+sol[2,:]+sol[3,:]
+G = sol[4,:]+sol[5,:]+sol[6,:]
+U = sol[7,:]+sol[8,:]+sol[9,:]
+
+S = sol[1,:]+sol[4,:]+sol[7,:]
+I_ = sol[2,:]+sol[5,:]+sol[8,:]
+R = sol[3,:]+sol[6,:]+sol[9,:]
+Θe = sol[10,:]
+Θg = sol[11,:]
+
+
+pe=plot([E_DF,E],label=[L"E^{DF}" L"E"],xlabel=L"t",legend=:outerright)
+pg=plot([G_DF,G],label=[L"G^{DF}" L"G"],xlabel=L"t",legend=:outerright)
+pu=plot([U_DF,U],label=[L"U^{DF}" L"U"],xlabel=L"t",legend=:outerright)
+psir=plot([S,I_,R],label=[L"S" L"I" L"R"],xlabel=L"t",legend=:outerright)
+
+ptightE=plot([Θe_DF,Θe],label=[L"\Theta_e^{DF}" L"\Theta_e"],xlabel=L"t",legend=:outerright)
+ptightG=plot([Θg_DF,Θg],label=[L"\Theta_g^{DF}" L"\Theta_g"],xlabel=L"t",legend=:outerright)
+
+plot(pe,pg,pu,psir,ptightE,ptightG,layout=(3,2))
+savefig("timeseries.pdf")
+
+# # Plot the effect of a government benefit to gig workers
+# prob = ODEProblem(epiecon_ode, u₀, tspan, p_gig)
+# sol = solve(prob,saveat=1)
+# E_gig = sol[1,:]+sol[2,:]+sol[3,:]
+# G_gig = sol[4,:]+sol[5,:]+sol[6,:]
+# U_gig = sol[7,:]+sol[8,:]+sol[9,:]
+
+# S_gig = sol[1,:]+sol[4,:]+sol[7,:]-S
+# I_gig = sol[2,:]+sol[5,:]+sol[8,:]-I
+# R_gig = sol[3,:]+sol[6,:]+sol[9,:]-R
+
+# Θe_gig = sol[10,:]
+# Θg_gig = sol[11,:]
+
+# pe_gig=plot([E_DF,E,E_gig],label=[L"E^{DF}" L"E" L"E^*"],xlabel=L"t",legend=:outerright)
+# pg_gig=plot([G_DF,G,G_gig],label=[L"G^{DF}" L"G" L"G^*"],xlabel=L"t",legend=:outerright)
+# pu_gig=plot([U_DF,U,U_gig],label=[L"U^{DF}" L"U" L"U^*"],xlabel=L"t",legend=:outerright)
+# psir_gig=plot([S_gig,I_gig,R_gig],label=[L"\Delta S" L"\Delta I" L"\Delta R"],xlabel=L"t",legend=:outerright)
+
+# ptightE_gig=plot([Θe_DF,Θe,Θe_gig],label=[L"\Theta_e^{DF}" L"\Theta_e" L"\Theta_e^*"],xlabel=L"t",legend=:outerright)
+# ptightG_gig=plot([Θg_DF,Θg,Θe_gig],label=[L"\Theta_g^{DF}" L"\Theta_g" L"\Theta_g^*"],xlabel=L"t",legend=:outerright)
+
+# plot(pe_gig,pg_gig,pu_gig,psir_gig,ptightE_gig,ptightG_gig,layout=(3,2))
+# savefig("timeseries_gig_benefits.pdf")
+
+# # Plot the effect of a government quarantine (λ₁>λ₂)
+# prob = ODEProblem(epiecon_ode, u₀, tspan, p_quar)
+# sol = solve(prob,saveat=1)
+# E_quar = sol[1,:]+sol[2,:]+sol[3,:]
+# G_quar = sol[4,:]+sol[5,:]+sol[6,:]
+# U_quar = sol[7,:]+sol[8,:]+sol[9,:]
+
+# S_quar = sol[1,:]+sol[4,:]+sol[7,:]-S
+# I_quar = sol[2,:]+sol[5,:]+sol[8,:]-I
+# R_quar = sol[3,:]+sol[6,:]+sol[9,:]-R
+
+# Θe_quar = sol[10,:]
+# Θg_quar = sol[11,:]
+
+# pe_quar=plot([E_DF,E,E_quar],label=[L"E^{DF}" L"E" L"E^*"],xlabel=L"t",legend=:outerright)
+# pg_quar=plot([G_DF,G,G_quar],label=[L"G^{DF}" L"G" L"G^*"],xlabel=L"t",legend=:outerright)
+# pu_quar=plot([U_DF,U,U_quar],label=[L"U^{DF}" L"U" L"U^*"],xlabel=L"t",legend=:outerright)
+# psir_quar=plot([S_quar,I_quar,R_quar],label=[L"\Delta S" L"\Delta I" L"\Delta R"],xlabel=L"t",legend=:outerright)
+
+# ptightE_quar=plot([Θe_DF,Θe,Θe_quar],label=[L"\Theta_e^{DF}" L"\Theta_e" L"\Theta_e^*"],xlabel=L"t",legend=:outerright)
+# ptightG_quar=plot([Θg_DF,Θg,Θe_quar],label=[L"\Theta_g^{DF}" L"\Theta_g" L"\Theta_g^*"],xlabel=L"t",legend=:outerright)
+
+# plot(pe_quar,pg_quar,pu_quar,psir_quar,ptightE_quar,ptightG_quar,layout=(3,2))
+# savefig("timeseries_quarantine.pdf")
+
+# function wage(yⱼ,βⱼ,I,Θ₁,Θ₂)
+#     α,βᵤ,b,d,c₁,c₂ = 0.5,0.2,0.71,1,0.1,0.1
+#     return α*yⱼ .+ (1-α)*(b .+ d*(βⱼ-βᵤ)*I) .+ α*c₁*Θ₁ .+ α*c₂*Θ₂
+# end
+
+# plot([wage(1.1,0.4,I,Θe,Θg),wage(1.0,0.3,I,Θe,Θg)],label=["We" "Wg"])
