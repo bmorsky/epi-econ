5ee6c116618a82512ed23ac99350449fcd11a9d4
Tyler Meadows <tam15@queensu.ca>
2025-03-07 15:42:28 -0500

    Sensitivity Analysis

--- Sensitivity.jl
+++ Sensitivity.jl
@@ -1,53 +1,50 @@
 using GlobalSensitivity, Statistics, QuasiMonteCarlo
 using Plots, LaTeXStrings
-theme(:default,
-    lw = 2,
-    size = (450,200),
-    fontfamily = "computer modern",)
 
-include("ExpandedModel.jl")
+
+include("model.jl")
 
 
 T = 1000.0
 
-# Initial Conditions
-u₀ = [0.905, 0.0, 0.0, 0.07, 0.0, 0.0, 0.025, 0.0, 0.0]
-# Economic Variable ICs [Θ₁,Θ₂,Vfes,Vfgs,Vfei,Vfgi,Vfer,Vfgi]
-v₀ = [2.5, 0.001, 25, 25, 25, 25, 25, 25] # Initial guess
+# 1% initially infected
+u₀ = [0.905*0.99, 0.905*0.01, 0.0, 0.07*0.99, 0.07*0.01, 0.0, 0.025*0.99, 0.025*0.01, 0.0, 2.8, 0.0165]
 
-v = nlsolve((F,x) -> equations!(F,x,p,u₀),v₀,ftol=1e-14)
-u = [u₀;v.zero]
-prob = ODEProblem(DAE, u, (0.0,T), p)
+prob = ODEProblem(epiecon, u₀,(0.0,T), p)
 #solve to steady state
-# Introduce an infection
-u₀ = [0.905*0.99, 0.905*0.01, 0.0, 0.07*0.99, 0.07*0.01, 0.0, 0.025*0.99, 0.025*0.01, 0.0]
 
 # Parameters to change
-keys = (:b,:λ)
-lbs = [0.01,0.0001] # Lower Bounds
-ubs = [0.75,0.01] # Upper Bounds
+keys = ["b","d","bg","λ₁","λ₂"]
+lbs = [0.1,0.25,0.0,0.0033,0.0033] # Lower Bounds
+ubs = [0.75,1.0,0.09,0.01,0.1] # Upper Bounds
 
 
 f1 = function(pars)
-    p_new = merge(p,NamedTuple{keys}(pars))
-    v = nlsolve((F,x) -> equations!(F,x,p_new,u₀),v₀,ftol=1e-14)
-    u = [u₀;v.zero]
-    prob = ODEProblem(DAE, u,(0.0,T),p_new)
-    sol = solve(prob,saveat = 0:T)
+    p_new = merge(p,NamedTuple{(Symbol.(keys)...,)}(pars))
+    prob = ODEProblem(epiecon, u₀,(0.0,T),p_new)
+    sol = solve(prob,saveat=1,reltol=1e-8)
     U = sum(sol[[7,8,9],:],dims=1)
-    return [maximum(U),U[end]]
+    Is = sum(sol[[2,5,8],:],dims=1)
+    return [maximum(U),U[end],maximum(Is),Is[end]]
 end
 
 
 A,B = QuasiMonteCarlo.generate_design_matrices(1000,lbs,ubs,LatinHypercubeSample())
 
-SobolIndices = gsa(f1,Sobol(order=[0,1,2]),A,B)
+SobolIndices = gsa(f1,Sobol(order=[0,1]),A,B)
 
-SobolIndices.S2
-b1 = bar([L"b",L"\lambda"],SobolIndices.S2[:], title = L"Effect on $U_{max}$", ylims=(0.0,1.0),legend=nothing)
-b2 = bar([L"b",L"\lambda"],SobolIndices.S2[2,:], title = L"Effect on $U_{\infty}$",ylims=(0.0,1.4),legend = nothing)
-plot(b1,b2, layout = (1,2))
+indices = [L"b",L"d",L"bg",L"\lambda_1",L"$\lambda_2$"]
 
+b1 = bar(indices,SobolIndices.S1[1,:], ylabel = "First order", title = L"$U_{max}$ Sobol Indices",legend=nothing, ylims = (0.0,1.0));
+b2 = bar(indices,SobolIndices.S1[2,:], title = L"$U_{\infty}$ Sobol Indices",legend = nothing,ylims=(0.0,1.0));
+b3 = bar(indices,SobolIndices.ST[1,:], ylabel = "Total order", legend=nothing,ylims=(0.0,1.0));
+b4 = bar(indices,SobolIndices.ST[2,:], legend=nothing,ylims=(0.0,1.0));
+plot(b1,b2,b3,b4 ,layout = (2,2))
 
+c1 = bar(indices,SobolIndices.S1[3,:], ylabel = "First order", title = L"$I_{max}$ Sobol Indices",legend=nothing, ylims = (0.0,1.0));
+c2 = bar(indices,SobolIndices.S1[4,:], title = L"$I_{\infty}$ Sobol Indices",legend = nothing,ylims=(0.0,1.0));
+c3 = bar(indices,SobolIndices.ST[3,:], ylabel = "Total order", legend=nothing,ylims=(0.0,1.0));
+c4 = bar(indices,SobolIndices.ST[4,:], legend=nothing,ylims=(0.0,1.0));
+plot(c1,c2,c3,c4, layout = (2,2))
 
 
