91be6c22759a58bc720ae25dea8d8b4490ae33fe
bmorsky <bryce.morsky@gmail.com>
2026-02-06 11:56:56 -0500

    added control problem for public policy

---
+++ dynamic optimization.jl
@@ -0,0 +1,73 @@
+using ModelingToolkit
+
+t = ModelingToolkit.t_nounits
+D = ModelingToolkit.D_nounits
+
+@parameters α,β₁,β₂,βᵤ,γ,η,λ₁,λ₂,μ,ρ,b₂,c₁,c₂,r,y₁,y₂
+@variables begin
+     S₁(..), [bounds=(0,1)]
+     I₁(..), [bounds=(0,1)]
+     R₁(..), [bounds=(0,1)]
+     S₂(..), [bounds=(0,1)]
+     I₂(..), [bounds=(0,1)]
+     R₂(..), [bounds=(0,1)]
+     Sᵤ(..), [bounds=(0,1)]
+     Iᵤ(..), [bounds=(0,1)]
+     Rᵤ(..), [bounds=(0,1)]
+     Θ₁(..), [bounds=(0,1000)]
+     Θ₂(..), [bounds=(0,1000)]
+     bᵤ(..), [input = true, bounds = (0, 1)]
+end
+
+P(Θ) = μ*(max(Θ, 1e-12))^(1-η)
+Q(Θ) = μ*(max(Θ, 1e-12))^(-η)
+
+eqs = [   D(S₁(t)) ~ ρ*R₁(t) - β₁*S₁(t)*(I₁(t)+I₂(t)+Iᵤ(t)) + P(Θ₁(t))*Sᵤ(t) - λ₁*S₁(t),
+          D(I₁(t)) ~ β₁*S₁(t)*(I₁(t)+I₂(t)+Iᵤ(t)) - (γ+λ₁)*I₁(t), #+ P(Θ₁)*Iᵤ
+          D(R₁(t)) ~ γ*I₁(t) - ρ*R₁(t) + P(Θ₁(t))*Rᵤ(t) - λ₁*R₁(t),
+          D(S₂(t)) ~ ρ*R₂(t) - β₂*S₂(t)*(I₁(t)+I₂(t)+Iᵤ(t)) + P(Θ₂(t))*Sᵤ(t) - λ₂*S₂(t),
+          D(I₂(t)) ~ β₂*S₂(t)*(I₁(t)+I₂(t)+Iᵤ(t)) - (γ+λ₂)*I₂(t), #+ P(Θ₂)*Iᵤ
+          D(R₂(t)) ~ γ*I₂(t) - ρ*R₂(t) + P(Θ₂(t))*Rᵤ(t) - λ₂*R₂(t),
+          D(Sᵤ(t)) ~ ρ*Rᵤ(t) - βᵤ*Sᵤ(t)*(I₁(t)+I₂(t)+Iᵤ(t)) - (P(Θ₁(t))+P(Θ₂(t)))*Sᵤ(t) + λ₁*S₁(t)+λ₂*S₂(t),
+          D(Iᵤ(t)) ~ βᵤ*Sᵤ(t)*(I₁(t)+I₂(t)+Iᵤ(t)) - γ*Iᵤ(t) + λ₁*I₁(t) + λ₂*I₂(t), # - (P(Θ₁)+P(Θ₂))*Iᵤ
+          0 ~ 1.0-S₁(t)-I₁(t)-R₁(t)-S₂(t)-I₂(t)-R₂(t)-Sᵤ(t)-Iᵤ(t)-Rᵤ(t),
+          0 ~ (1-α)*(y₁*((S₁(t)+R₁(t))/(S₁(t)+I₁(t)+R₁(t))) - bᵤ(t)) - α*c₁*Θ₁(t) - α*c₂*Θ₂(t) - c₁*(r+λ₁)/Q(Θ₁(t)),
+          0 ~ (1-α)*(y₂*((S₂(t)+R₂(t))/(S₂(t)+I₂(t)+R₂(t))) + b₂ - bᵤ(t)) - α*c₁*Θ₁(t) - α*c₂*Θ₂(t) - c₂*(r+λ₂)/Q(Θ₂(t))]
+
+(ts, te) = (0.0, 600.0)
+costs = [I₁(te)+I₂(te)+Iᵤ(te)]#[(Sᵤ(t)+Iᵤ(t)+Rᵤ(t))^2 + (I₁(t)+I₂(t)+Iᵤ(t))^2 + (bᵤ(t))^2]
+cons = [bᵤ(te) ~ 0.4]
+
+@named epieconsys = System(eqs, t; costs, constraints = cons)
+epieconsys = mtkcompile(epieconsys, inputs = [bᵤ(t)])
+
+include("model.jl")
+
+# Disease-free scenario
+u₀ = [SF₀, 0.0, 0.0, SG₀, 0.0, 0.0, SU₀, 0.0, 0.0, Θ₁₀, Θ₂₀]
+prob = ODEProblem(epiecon, u₀, tspan, p)
+sol = solve(prob,saveat=0.1, RadauIIA5(), reltol=1e-8, abstol=1e-8)
+F_DF = sol[1,:]+sol[2,:]+sol[3,:]
+G_DF = sol[4,:]+sol[5,:]+sol[6,:]
+U_DF = 1.0 .- F_DF .- G_DF
+Θf_DF = sol[10,:]
+Θg_DF = sol[11,:]
+Wf_DF = W₁(0,Θf_DF,Θg_DF,p)
+Wg_DF = W₂(0,Θf_DF,Θg_DF,p)
+SW_DF = Wf_DF.*F_DF + p.bᵤ.*U_DF + (Wg_DF .+ p.b₂).*G_DF
+
+# 1% initially infected
+u₀ = [SF₀*0.99, SF₀*0.01, 0.0, SG₀*0.99, SG₀*0.01, 0.0, SU₀*0.99, SU₀*0.01, 0.0, Θ₁₀, Θ₂₀]
+
+u0map = [ S₁(t) => SF₀*0.99, I₁(t) => SF₀*0.01, R₁(t) => 0.0, 
+          S₂(t) => SG₀*0.99, I₂(t) => SG₀*0.01, R₂(t) => 0.0,
+          Sᵤ(t) => SU₀*0.99, Iᵤ(t) => SU₀*0.01, Rᵤ(t) => 0.0,
+          Θ₁(t) => Θ₁₀, Θ₂(t) => Θ₂₀, bᵤ(t) => 0.4]
+
+pmap = [α => 0.5, β₁ => 0.4, β₂ => 0.38, βᵤ => 0.36, γ => 1/5, η => 0.5, λ₁ => 0.001096, λ₂ => 0.005479,
+     μ => 0.01517, ρ => 0.01, b₂ => 0.2, bᵤ => 0.4, c₁ => 0.17305498516997503, c₂ => 0.07253458196414889,
+     r => 0.0001337, y₁ => 1.014065708418891, y₂ => 0.914065708418891]
+
+using InfiniteOpt, Ipopt, DiffEqDevTools
+jprob = JuMPDynamicOptProblem(epieconsys, [u0map; pmap], (ts, te); dt = 0.001)
+jsol = solve(jprob, JuMPCollocation(Ipopt.Optimizer, constructRadauIIA5()));
--- epiecon.jl
+++ epiecon.jl
@@ -19,6 +19,7 @@ U_DF = 1.0 .- F_DF .- G_DF
 Θg_DF = sol[11,:]
 Wf_DF = W₁(0,Θf_DF,Θg_DF,p)
 Wg_DF = W₂(0,Θf_DF,Θg_DF,p)
+SW_DF = Wf_DF.*F_DF + p.bᵤ.*U_DF + (Wg_DF .+ p.b₂).*G_DF
 
 # 1% initially infected
 u₀ = [SF₀*0.99, SF₀*0.01, 0.0, SG₀*0.99, SG₀*0.01, 0.0, SU₀*0.99, SU₀*0.01, 0.0, Θ₁₀, Θ₂₀]
@@ -34,6 +35,7 @@ I = sol[2,:]+sol[5,:]+sol[8,:]
 R = 1.0 .- S .- I
 Wf = W₁(sol[2,:] ./ F,Θf,Θg,p)
 Wg = W₂(sol[5,:] ./ G,Θf,Θg,p)
+SW = Wf.*F + p.bᵤ.*U + (Wg.*(1 .- sol[5,:]./G) .+ p.b₂).*G
 t = sol.t
 
 pf=plot(t,[F_DF,F],label=[L"F^{DF}" L"F"],xlabel="",legend=:outerright)
@@ -44,8 +46,9 @@ ptightE=plot(t,[Θf_DF,Θf],label=[L"\Theta_f^{DF}" L"\Theta_f"],xlabel="",legen
 ptightG=plot(t,[Θg_DF,Θg],label=[L"\Theta_g^{DF}" L"\Theta_g"],xlabel="",legend=:outerright)
 pwf=plot(t,[Wf_DF,Wf],label=[L"W_f^{DF}" L"W_f"],xlabel="time (days)",legend=:outerright)
 pwg=plot(t,[Wg_DF,Wg],label=[L"W_g^{DF}" L"W_g"],xlabel="time (days)",legend=:outerright)
+psw=plot(t,[SW_DF,SW],label=L"SW",xlabel="time (days)",legend=:outerright)
 
-plot(pf,pg,pu,psir,ptightE,ptightG,pwf,pwg,layout=(4,2))
+plot(pf,pg,pu,psir,ptightE,ptightG,pwf,pwg,psw,layout=(5,2),size=(600, 800))
 savefig("timeseries.pdf")
 
 # Plot the effect of a government benefit to gig workers
@@ -64,6 +67,7 @@ I_gig = 100*I_gig ./ I
 R_gig = 100*R_gig ./ R
 Wf_gig = W₁(sol[2,:] ./ F_gig,Θf_gig,Θg_gig,p_gig)
 Wg_gig = W₂(sol[5,:] ./ G_gig,Θf_gig,Θg_gig,p_gig)
+SW_gig = Wf_gig.*F + p.bᵤ.*U_gig + (Wg_gig.*(1 .- sol[5,:]./G_gig) .+ p.b₂).*G_gig
 t = sol.t
 
 pf_gig=plot(t,[F_DF,F,F_gig],label=[L"F^{DF}" L"F" L"F^B"],xlabel="",legend=:outerright)
@@ -74,8 +78,9 @@ ptightF_gig=plot(t,[Θf_DF,Θf,Θf_gig],label=[L"\Theta_f^{DF}" L"\Theta_f" L"\T
 ptightG_gig=plot(t,[Θg_DF,Θg,Θg_gig],label=[L"\Theta_g^{DF}" L"\Theta_g" L"\Theta_g^B"],xlabel="",legend=:outerright)
 pwf_gig=plot(t,[Wf_DF,Wf_gig],label=[L"W_f^{DF}" L"W_f^B"],xlabel="time (days)",legend=:outerright)
 pwg_gig=plot(t,[Wg_DF,Wg_gig],label=[L"W_g^{DF}" L"W_g^B"],xlabel="time (days)",legend=:outerright)
+psw_gig=plot(t,[SW_DF,SW_gig],label=L"SW",xlabel="time (days)",legend=:outerright)
 
-plot(pf_gig,pg_gig,pu_gig,psir_gig,ptightF_gig,ptightG_gig,pwf_gig,pwg_gig,layout=(4,2))
+plot(pf_gig,pg_gig,pu_gig,psir_gig,ptightF_gig,ptightG_gig,pwf_gig,pwg_gig,psw_gig,layout=(5,2),size=(600, 800))
 savefig("timeseries_gig_benefits.pdf")
 
 # Plot the effect of a higher job losses in the formal economy
@@ -94,6 +99,7 @@ I_jloss = 100*I_jloss ./ I
 R_jloss = 100*R_jloss ./ R
 Wf_jloss = W₁(sol[2,:] ./ F_jloss,Θf_jloss,Θg_jloss,p_jloss)
 Wg_jloss = W₂(sol[5,:] ./ G_jloss,Θf_jloss,Θg_jloss,p_jloss)
+SW_jloss = Wf_jloss.*F_jloss + p.bᵤ.*U_jloss + (Wg_jloss.*(1 .- sol[5,:]./G_jloss) .+ p.b₂).*G_jloss
 t = sol.t
 
 pf_jloss=plot(t,[F_DF,F,F_jloss],label=[L"F^{DF}" L"F" L"F^L"],xlabel="",legend=:outerright,)
@@ -104,8 +110,9 @@ ptightF_jloss=plot(t,[Θf_DF,Θf,Θf_jloss],label=[L"\Theta_f^{DF}" L"\Theta_f" 
 ptightG_jloss=plot(t,[Θg_DF,Θg,Θg_jloss],label=[L"\Theta_g^{DF}" L"\Theta_g" L"\Theta_g^L"],xlabel="",legend=:outerright)
 pwf_jloss=plot(t,[Wf_DF,Wf_jloss],label=[L"W_f^{DF}" L"W_f^L"],xlabel="time (days)",legend=:outerright)
 pwg_jloss=plot(t,[Wg_DF,Wg_jloss],label=[L"W_g^{DF}" L"W_g^L"],xlabel="time (days)",legend=:outerright)
+psw_jloss=plot(t,[SW_DF,SW_jloss],label=L"SW",xlabel="time (days)",legend=:outerright)
 
-plot(pf_jloss,pg_jloss,pu_jloss,psir_jloss,ptightF_jloss,ptightG_jloss,pwf_jloss,pwg_jloss,layout=(4,2))
+plot(pf_jloss,pg_jloss,pu_jloss,psir_jloss,ptightF_jloss,ptightG_jloss,pwf_jloss,pwg_jloss,psw_jloss,layout=(5,2),size=(600, 800))
 savefig("timeseries_lambdaf.pdf")
 
 # Plot the effect of a higher bᵤ
@@ -124,6 +131,7 @@ I_bu = 100*I_bu ./ I
 R_bu = 100*R_bu ./ R
 Wf_bu = W₁(sol[2,:] ./ F_bu,Θf_bu,Θg_bu,p_bu)
 Wg_bu = W₂(sol[5,:] ./ G_bu,Θf_bu,Θg_bu,p_bu)
+SW_bu = Wf_bu.*F + p.bᵤ.*U_bu + (Wg_bu.*(1 .- sol[5,:]./G_bu) .+ p.b₂).*G_bu
 t = sol.t
 
 pf_bu=plot(t,[F_DF,F,F_bu],label=[L"F^{DF}" L"F" L"F^B"],xlabel="",legend=:outerright,)
@@ -134,6 +142,37 @@ ptightF_bu=plot(t,[Θf_DF,Θf,Θf_bu],label=[L"\Theta_f^{DF}" L"\Theta_f" L"\The
 ptightG_bu=plot(t,[Θg_DF,Θg,Θg_bu],label=[L"\Theta_g^{DF}" L"\Theta_g" L"\Theta_g^B"],xlabel="",legend=:outerright)
 pwf_bu=plot(t,[Wf_DF,Wf_bu],label=[L"W_f^{DF}" L"W_f^B"],xlabel="time (days)",legend=:outerright)
 pwg_bu=plot(t,[Wg_DF,Wg_bu],label=[L"W_g^{DF}" L"W_g^B"],xlabel="time (days)",legend=:outerright)
+psw_bu=plot(t,[SW_DF,SW_bu],label=L"SW",xlabel="time (days)",legend=:outerright)
 
-plot(pf_bu,pg_bu,pu_bu,psir_bu,ptightF_bu,ptightG_bu,pwf_bu,pwg_bu,layout=(4,2))
+plot(pf_bu,pg_bu,pu_bu,psir_bu,ptightF_bu,ptightG_bu,pwf_bu,pwg_bu,psw_bu,layout=(5,2),size=(600, 800))
 savefig("timeseries_bu.pdf")
+
+# Alternative assumption β₂ > β₁
+u₀ = [SF₀*0.99, SF₀*0.01, 0.0, SG₀*0.99, SG₀*0.01, 0.0, SU₀*0.99, SU₀*0.01, 0.0, Θ₁₀, Θ₂₀]
+prob = ODEProblem(epiecon, u₀, tspan, p_alt)
+sol = solve(prob,saveat=0.1, RadauIIA5(), reltol=1e-8, abstol=1e-8)
+F_alt = sol[1,:]+sol[2,:]+sol[3,:]
+G_alt = sol[4,:]+sol[5,:]+sol[6,:]
+U_alt = 1.0 .- F_alt .- G_alt
+Θf_alt = sol[10,:]
+Θg_alt = sol[11,:]
+S_alt = sol[1,:]+sol[4,:]+sol[7,:]
+I_alt = sol[2,:]+sol[5,:]+sol[8,:]
+R_alt = 1.0 .- S_alt .- I_alt
+Wf_alt = W₁(sol[2,:] ./ F_alt,Θf_alt,Θg_alt,p_alt)
+Wg_alt = W₂(sol[5,:] ./ G_alt,Θf_alt,Θg_alt,p_alt)
+SW_alt = Wf_alt.*F_alt + p_alt.bᵤ.*U_alt + (Wg_alt.*(1 .- sol[5,:]./G_alt) .+ p_alt.b₂).*G_alt
+t = sol.t
+
+pf_alt=plot(t,[F_DF,F_alt],label=[L"F^{DF}" L"F"],xlabel="",legend=:outerright)
+pg_alt=plot(t,[G_DF,G_alt],label=[L"G^{DF}" L"G"],xlabel="",legend=:outerright)
+pu_alt=plot(t,[U_DF,U_alt],label=[L"U^{DF}" L"U"],xlabel="",legend=:outerright)
+psir_alt=plot(t,[S_alt,I_alt,R_alt],label=[L"S" L"I" L"R"],xlabel="",legend=:outerright)
+ptightE_alt=plot(t,[Θf_DF,Θf_alt],label=[L"\Theta_f^{DF}" L"\Theta_f"],xlabel="",legend=:outerright)
+ptightG_alt=plot(t,[Θg_DF,Θg_alt],label=[L"\Theta_g^{DF}" L"\Theta_g"],xlabel="",legend=:outerright)
+pwf_alt=plot(t,[Wf_DF,Wf_alt],label=[L"W_f^{DF}" L"W_f"],xlabel="time (days)",legend=:outerright)
+pwg_alt=plot(t,[Wg_DF,Wg_alt],label=[L"W_g^{DF}" L"W_g"],xlabel="time (days)",legend=:outerright)
+psw_alt=plot(t,[SW_DF,SW_alt],label=L"SW",xlabel="time (days)",legend=:outerright)
+
+plot(pf_alt,pg_alt,pu_alt,psir_alt,ptightE_alt,ptightG_alt,pwf_alt,pwg_alt,psw_alt,layout=(5,2),size=(600, 800))
+savefig("timeseries_alt.pdf")
